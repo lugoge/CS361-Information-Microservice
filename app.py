4db387a60baf1b3ac2bb45e650dfdf28c4767901
lugoge <lugoge@oregonstate.edu>
2026-05-12 13:52:19 -0700

    Added initila code, and starter data base

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify
+import json
+import os
+
+app = Flask(__name__)
+
+DATA_FILE = "data.json"
+
+def load_help_data():
+    """Load help hints form JSON file."""
+    if not os.path.exists(DATA_FILE):
+        return()
+    
+    with open(DATA_FILE, "r") as file:
+        return json.load(file)
+
+# Pulls contextual help tip from data base
+@app.route("/info/context>", methods=["GET"])
+def get_info(context):
+    help_data = load_help_data()
+
+    if context not in help_data:
+        return jsonify({
+            "error": "No help infromation found for this context."
+        }), 404
+    
+    response = {
+        "context": context, **help_data[context]
+    }
+
+    return jsonify(response), 200
+
+# Can pull up all helpful tips, if just sending "/info" GET.
+@app.route("/info", method=["GET"])
+def list_available_contexts():
+    load_help_data = load_help_data()
+    return jsonify({
+        "available_contexts": list(help_data.keys())
+    }), 200
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5004, debug=True)
